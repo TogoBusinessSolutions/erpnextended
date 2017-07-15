47cb3fadee8a0bbef9ccf2675f65227f240c07fa
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-07-15 06:44:02 -0700

    Updated js

--- erpnextended/erpnext_extended_functions/doctype/employee_payment_allocation/employee_payment_allocation.js
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment_allocation/employee_payment_allocation.js
@@ -1,18 +1,19 @@
 frappe.ui.form.on('Employee Payment Allocation', {
-        employee_payment_allocation: function{frm, cdt, cdn){
-                var employee_payment_allocation = frappe.model.get_doc(cdt, cdn);
-                if(employee_payment_allocation.employee){
-                        frm.call({
-                                method: "erpnext_extended_functions.employee_payment.doctype$
-                                args: {
-                                        employee: employee_allocations.employee
-                                },
-                                callback: function(r){
-                                        frappe.model.set_value(cdt, cdn, "full_name", r.mess$
-                                }
-                        });
-                }else{
-                        frappe.model.set_value(cdt, cdn, "full_name", null);
-                }
-        }
+	employee_payment_allocation: function{frm, cdt, cdn){
+		var employee_payment_allocation = frappe.model.get_doc(cdt, cdn);
+		if(employee_payment_allocation.employee){
+			frm.call({
+				method: 
+"erpnext_extended_functions.employee_payment.doctype.erpnext_extended_functions.employee_payment.get_full_name",
+				args: {
+					employee: employee_allocations.employee
+				},
+				callback: function(r){
+					frappe.model.set_value(cdt, cdn, "full_name", r.message);
+				}
+			});
+		}else{
+			frappe.model.set_value(cdt, cdn, "full_name", null);
+		}
+	}
 });
