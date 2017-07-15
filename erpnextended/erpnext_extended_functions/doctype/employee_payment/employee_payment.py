b5b66b7516a39cc3f67394e6ee4cbca42817c7d8
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-07-15 06:27:47 -0700

    added docs

--- erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.py
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.py
@@ -7,4 +7,13 @@ import frappe
 from frappe.model.document import Document
 
 class EmployeePayment(Document):
-	pass
+	"""Set full name"""
+	for allocation in self.employee_allocations:
+		if not allocation.full_name:
+			allocation.full_name = get_full_name(allocation.employee)
+
+@frappe.whitelist()
+def get_full_name(employee):
+	emp = frappe.get_doc("Employee",employee)
+	return " ".join(filter(None,[emp.first_name,emp.last_name]))
+
