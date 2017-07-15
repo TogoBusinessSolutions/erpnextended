85d025c599d76e5c6ed1e2c53763126273b52542
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-07-15 06:31:59 -0700

    Updated js

--- erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.js
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.js
@@ -1,22 +1,3 @@
 // Copyright (c) 2017, Togo Business Solutions and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Employee Payment Allocation', {
-	employee_payment_allocation: function{frm, cdt, cdn){
-		var employee_payment_allocation = frappe.model.get_doc(cdt, cdn);
-		if(employee_payment_allocation.employee){
-			frm.call({
-				method: "erpnext_extended_functions.employee_payment.doctype.erpnext_extended_functions.employee_payment.get_full_name",
-				args: {
-					employee: employee_allocations.employee
-				},
-				callback: function(r){
-					frappe.model.set_value(cdt, cdn, "full_name", r.message); 
-				}
-			});
-		}else{
-			frappe.model.set_value(cdt, cdn, "full_name", null);
-		}
-	}
-});
-
