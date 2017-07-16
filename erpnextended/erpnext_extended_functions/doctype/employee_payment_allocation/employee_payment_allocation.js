1536e6c87c199404bf9b2488bbc983e7c38e741a
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-07-16 03:29:37 -0700

    Updated validate

--- erpnextended/erpnext_extended_functions/doctype/employee_payment_allocation/employee_payment_allocation.js
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment_allocation/employee_payment_allocation.js
@@ -3,21 +3,13 @@ frappe.ui.form.on('Employee Payment Allocation', {
 		var employee_payment_allocation = frappe.model.get_doc(cdt, cdn);
 		if(employee_payment_allocation.employee){
 			frm.call({
-<<<<<<< HEAD
 				method: 
 "erpnext_extended_functions.employee_payment.doctype.erpnext_extended_functions.employee_payment.get_full_name",
-=======
-				method: "erpnext_extended_functions.employee_payment.doctype.erpnext_extended_functions.employee_payment.get_full_name",
->>>>>>> 446b7d4285e003fda741fefb5c7fd9502f1b0394
 				args: {
 					employee: employee_allocations.employee
 				},
 				callback: function(r){
-<<<<<<< HEAD
-					frappe.model.set_value(cdt, cdn, "full_name", r.message);
-=======
 					frappe.model.set_value(cdt, cdn, "full_name", r.message); 
->>>>>>> 446b7d4285e003fda741fefb5c7fd9502f1b0394
 				}
 			});
 		}else{
