dfcf6786d323aba5f56833c9a80721f057c28094
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-07-16 03:25:21 -0700

    Updated validate

--- erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.py
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.py
@@ -7,7 +7,6 @@ import frappe
 from frappe.model.document import Document
 
 class EmployeePayment(Document):
-<<<<<<< HEAD
 	def validate(self):
 		"""Set full name"""
 		for allocation in self.employee_allocations:
@@ -18,16 +17,3 @@ class EmployeePayment(Document):
 def get_full_name(employee):
 	emp = frappe.get_doc("Employee",employee)
 	return emp.employee_name
-=======
-        def validate(self):
-                """Set full name"""
-                for allocation in self.employee_allocations:
-                        if not allocation.full_name:
-                                allocation.full_name = get_full_name(allocation.employee)
-
-@frappe.whitelist()
-def get_full_name(employee):
-        emp = frappe.get_doc("Employee",employee)
-        return emp.employee_name
-
->>>>>>> 446b7d4285e003fda741fefb5c7fd9502f1b0394
