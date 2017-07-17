07ee61b9a30499fa8fb87975cc6cd970086d76b5
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-07-17 14:38:49 -0700

    Updated validate

---
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment_allowed_accounts/employee_payment_allowed_accounts.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Togo Business Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Payment Allowed Accounts', {
+	refresh: function(frm) {
+
+	}
+});
