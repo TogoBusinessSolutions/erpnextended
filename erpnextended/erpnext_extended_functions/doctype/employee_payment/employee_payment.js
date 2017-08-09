bb1992a8d153f903ee9f0c67ade622273476c237
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-08-09 06:38:26 -0700

    Fill Name working

--- erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.js
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Payment Allocation', {
-        employee_allocations_all: function(frm, cdt, cdn){
+        employee: function(frm, cdt, cdn){
                 var employee_payment_allocation = frappe.model.get_doc(cdt, cdn);
 		if(employee_payment_allocation.employee){
                         frm.call({
@@ -18,5 +18,14 @@ frappe.ui.form.on('Employee Payment Allocation', {
                 }else{
                         frappe.model.set_value(cdt, cdn, "full_name", null);
                 }
+        }
+});
+
+frappe.ui.form.on('Employee Payment', {
+        journal_entry: function(frm, cdt, cdn){
+                //var journal = frappe.model.get_doc(cdt, cdn);
+                alert(frm.doc.journal_entry);
+		//frappe.msgprint("{0} save the doctype ", [journal.name]);
+                //frappe.model.set_value(cdt, cdn, "amount", journal.total_amount);
         }
 });
--- erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.py
+++ erpnextended/erpnext_extended_functions/doctype/employee_payment/employee_payment.py
@@ -13,9 +13,28 @@ class EmployeePayment(Document):
 		for allocation in self.employee_allocations:
 			if not allocation.full_name:
 				allocation.full_name = get_full_name(allocation.employee)
+		validate_account_allowed(self.journal_entry)
 
 @frappe.whitelist()
 def get_full_name(employee):
 	emp = frappe.get_doc("Employee",employee)
 	return emp.employee_name
+
+@frappe.whitelist()
+def get_journal_amount(journal_entry):
+	jrn = frappe.get_doc("Journal Entry",journal_entry)
+	return jrn.total_amount
+
+def validate_account_allowed(journal_entry):
+	"""Check if the selected account is in the allowed list"""
+	allowed_accounts = frappe.get_all('Employee Payment Allowed Accounts', fields=['allowed_account'])
+	journal_accounts = frappe.get_all('Journal Entry Account', filters={'parent': journal_entry}, fields=['account'])
+	found = False
+	for allowed_account in allowed_accounts:
+		for journal_account in journal_accounts:
+		 	if (allowed_account['allowed_account'] == journal_account['account']):		
+				found = True
+				break
+	if not found:
+		frappe.throw(_("Journal {0} is not a valid salary entry.").format(journal_entry))
 
