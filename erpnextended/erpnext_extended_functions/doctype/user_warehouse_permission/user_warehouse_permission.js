c3b259abc6066284c25269a8c921ab748eee63eb
Satish Ajodha <umhlaba.wethu@gmail.com>
2017-08-09 14:22:33 -0700

    Start Warehouse Permissions

---
+++ erpnextended/erpnext_extended_functions/doctype/user_warehouse_permission/user_warehouse_permission.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Togo Business Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('User Warehouse Permission', {
+	refresh: function(frm) {
+
+	}
+});
