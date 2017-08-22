a71ee36e8a0328e2771c7cd5bd31411b831a90c5
Satish Ajodha <satish@togosolutions.co.za>
2017-08-21 18:35:17 -0700

    Ready

---
+++ erpnextended/config/accounts.py
@@ -0,0 +1,32 @@
+
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Employee Payments"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Employee Payment",
+					"label": _("Employee Payment"),
+					"description": _("Allocate Employee payments"),
+				},
+				{
+					"type": "report",
+					"name": "Employee Payments",
+					"doctype": "Employee Payment",
+					"is_query_report": True,
+					"description": _("Employee Payments Summary")
+				},
+				{
+                                        "type": "report",
+                                        "name": "Unallocated Salary Journals",
+                                        "doctype": "Journal Entry",
+                                        "is_query_report": True,
+					"description": _("Unallocated Salary Journals")
+                                }
+			]
+		}
+	]
