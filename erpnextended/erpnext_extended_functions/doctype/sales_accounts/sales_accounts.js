438ff91deb1bde308112b1120094ee5f94302864
root <root@togoerpn-01.c.erpnext-pfs.internal>
2018-12-01 18:51:52 +0200

    Added Sales accounts

---
+++ erpnextended/erpnext_extended_functions/doctype/sales_accounts/sales_accounts.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Togo Business Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sales Accounts', {
+	refresh: function(frm) {
+
+	}
+});
